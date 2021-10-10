45f32ce33a73cbcfe65de393c88f3687be9413fc
Ester CardosoToja <63560521+estertoja@users.noreply.github.com>
2021-10-10 13:58:50 -0300

    Adicionando Imagens
    Adicionando Imagens

---
+++ myscript.js
@@ -0,0 +1,4 @@
+var peca = '<img src="imagens/black.png" height="42" width="42" style="vertical-align: middle;">';
+
+var pecawhite = '<img src="imagens/white.png" height="42" width="42" style="vertical-align: middle;">';
+
