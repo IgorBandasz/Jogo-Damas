08a94407153af4755a7196b0ab0e66c7b79df2cd
rohju <roberta.dias@rede.ulbra.br>
2021-10-16 11:23:45 -0300

    Create README-DESENVOLVIMENTO.mk

---
+++ README-DESENVOLVIMENTO.mk
@@ -0,0 +1,14 @@
+# COMO FOI DESENVOLVIDO:
+
+## PARA ORGANIZAÇÃO DO TRABALHO:
+* **Google Meet** - para as reuniões semanais e alinhamento dos integrantes.
+* **Trello** - para organização das responsabilidade e acompanhamento do progresso de desenvolvimento.
+* **GitHub** - para acesso compartilhado dos códigos desenvolvidos.
+## FRONT-END:
+* **HTML5** - utilizada para marcação e organização dos espaços do jogo em tela.
+* **CSS** - em conjunto com o html auxiliando na aparência e estilo dos itens adicionados.
+* **Markdown** - utilizada para desenvolvimento do README.
+## BACK- END:
+* **JavaScript** - precisamente aqui é onde a mágica acontece, onde as regras dos jogo são traduzidas para a linguagem de programação.
+## IDE: 
+* **Visual Studio Code** - espaço para colocar a mão na massa no desenvolvimento do front e back end. 
