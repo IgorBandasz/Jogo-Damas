1e12c2ae810a5e33145ab03c8cd6ce3c93813f4a
rohju <roberta.dias@rede.ulbra.br>
2021-10-16 09:47:17 -0300

    Create README-REGRAS.mk

---
+++ README-REGRAS.mk
@@ -0,0 +1,32 @@
+# REGRAS DO JOGO:
+
+## OBJETIVO DO JOGO: capturar ou imobilizar as peças do adversário. O jogador que conseguir capturar todas as peças do inimigo ganha a partida.
+
+## Regras Tabuleiro:
+* O jogo de damas pratica-se entre dois jogadores, num tabuleiro quadrado, de 64 casas alternadamente claras e escuras, dispondo de 12 peças brancas e 12 pretas.
+* O tabuleiro deve ser colocado de modo que a casa angular à esquerda de cada parceiro seja escura.
+* No início da partida, as peças devem ser colocadas no tabuleiro sobre as casas escuras, da seguinte forma: nas três primeiras filas horizontais, as peças brancas; e, nas três últimas, as peças pretas.
+
+## Regras de Movimentação:
+* A peça movimenta-se em diagonal, sobre as casas escuras, para a frente, e uma casa de cada vez.
+* A peça pode capturar a peça do adversário movendo-se para frente na diagonal.
+* A peça que atingir a oitava casa adversária, parando ali, será promovida a "dama", peça de movimentos mais amplos que a simples peça. Assinala-se a dama sobrepondo, à pedra promovida, outra da mesma cor
+* A dama pode mover-se para trás e para frente em diagonal uma casa de cada vez, diferente das outras peças, que movimentam-se apenas para frente em diagonal. A dama pode também tomar outra peça pela frente ou por trás em diagonal.
+* Quando na casa contígua a uma peça houver uma peça adversária, com uma casa imediata vaga, na mesma diagonal, a peça toma-la-á passando para a citada casa vaga. Se após a tomada de uma peça, a circunstância se repetir, a peça continuará a tomada no mesmo lance, chamando-se a este movimento tomada em cadeia.
+
+## Demais Regras:
+* A **tomada é obrigatória**. A peça e a dama têm o mesmo valor para tomar ou ser tomada. Se, no mesmo lance, se apresentar mais de um modo de tomar, é obrigatório executar o lance que tome o maior número de peças (lei da maioria).
+* Não será promovida a peça que, numa tomada em cadeia, apenas passe pela oitava casa adversária. 
+* As peças tomadas só deverão ser retiradas do tabuleiro depois de completo o lance.
+* As **brancas têm sempre a saída**, isto é, o primeiro lance da partida. Determina-se por sorteio ou convenção, para a primeira partida; nas seguintes, as brancas cabem alternadamente aos dois parceiros.
+* O lance é executado quando se leva diretamente à nova casa a peça tocada; a peça deve ser imediatamente solta.
+* O lance está completo quando a mão do jogador tiver largado a peça, ao movê-la de uma casa para outra.
+* Nota: se nenhuma dessas peças puder ser jogada legalmente, não haverá penalidade, entretanto, a reincidência é passível de punição.
+
+## A partida termina com ganhador quando:
+Um dos lados elimina todas as peças do adversário.
+
+## A partida termina empatada quando:
+A partir de qualquer ponto da partida, ocorrer 10 lances sucessivos sem tomada de peças ou Damas;
+
+
